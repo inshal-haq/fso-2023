eab1542455fcfdcbd861a1ec7b458765116d5e5a
Inshal Haq <inshalulhaq11@gmail.com>
2023-11-14 19:21:06 -0600

    Part4 Ex4.6* - helper functions and unit tests step4

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -4,25 +4,63 @@ const emptyList = []
 
 const listWithOneBlog = [
   {
-    title: 'Go To Statement Considered Harmful',
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 10
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
   }
 ]
 
 const listWithManyBlogs = [
   {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 10
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
   },
   {
+    _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
   }
 ]
 
@@ -39,20 +77,20 @@ describe('total likes', () => {
 
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(10)
+    expect(result).toBe(12)
   })
 
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithManyBlogs)
-    expect(result).toBe(15)
+    expect(result).toBe(36)
   })
 })
 
 describe('favorite blog', () => {
   const mostLikedBlog = {
-    title: 'Go To Statement Considered Harmful',
+    title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
-    likes: 10
+    likes: 12
   }
 
   test('of empty list is null', () => {
@@ -68,5 +106,31 @@ describe('favorite blog', () => {
   test('of a bigger list is selected right', () => {
     const result = listHelper.favoriteBlog(listWithManyBlogs)
     expect(result).toEqual(mostLikedBlog)
+  })
+})
+
+describe('most blogs', () => {
+  const oneBlogResult = {
+    author: 'Edsger W. Dijkstra',
+    blogs: 1
+  }
+  const multipleBlogsResult = {
+    author: 'Robert C. Martin',
+    blogs: 3
+  }
+
+  test('of empty list is null', () => {
+    const result = listHelper.mostBlogs(emptyList)
+    expect(result).toEqual(null)
+  })
+
+  test('when list has only one blog', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog)
+    expect(result).toEqual(oneBlogResult)
+  })
+
+  test('of a bigger list is selected right', () => {
+    const result = listHelper.mostBlogs(listWithManyBlogs)
+    expect(result).toEqual(multipleBlogsResult)
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = () => {
   return 1
 }
@@ -24,6 +26,24 @@ const favoriteBlog = blogs => {
   return mostLikedBlog
 }
 
+// returns the author who has the largest amount of blogs.
+// The return value also contains the number of blogs the top author has
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return null
+
+  const authorBlogCounts = _.countBy(blogs, 'author')
+
+  const mostBlogsAuthor = (Object.keys(authorBlogCounts)
+    .reduce((a, b) => authorBlogCounts[a] > authorBlogCounts[b] ? a : b))
+
+  const result = {
+    author: mostBlogsAuthor,
+    blogs: authorBlogCounts[mostBlogsAuthor]
+  }
+
+  return result
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
